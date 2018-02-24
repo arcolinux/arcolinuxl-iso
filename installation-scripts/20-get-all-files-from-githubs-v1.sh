559f1d2cd30dbf6726b2728fb76f9cf6c70cbf75
Erik Dubois <erik.dubois@gmail.com>
2018-02-24 22:57:18 +0100

    prior to building 6.5.2

--- installation-scripts/20-get-all-files-from-githubs-v1.sh
+++ installation-scripts/20-get-all-files-from-githubs-v1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#set -e
+set -e
 #
 ##################################################################################################################
 # Written to be used on 64 bits computers
 # Author 	: 	Erik Dubois
-# Archmerge : 	https://archmerge.com/
+# ArcoLinux : 	https://arcolinux.com/
 ##################################################################################################################
 ##################################################################################################################
 #
@@ -16,36 +16,36 @@ echo "Removing old files/folders from folder"
 rm -rf ../archiso/airootfs/etc/skel/.* 2> /dev/null
 
 echo "getting .bashrc from iso-skel"
-cp -r ../../Archmerge-Root/root/.bashrc-latest ../archiso/airootfs/etc/skel/.bashrc
+cp -r ../../arcolinux-root/root/.bashrc-latest ../archiso/airootfs/etc/skel/.bashrc
 echo ".bashrc copied to /etc/skel"
 
-echo "getting oblogout.conf from archmerge-oblogout-themes"
-cp -r ../../Archmerge-Oblogout-Themes/oblogout.conf.archmergenew ../archiso/airootfs/etc/oblogout.conf
+echo "getting oblogout.conf from arcolinux-oblogout-themes"
+cp -r ../../arcolinux-oblogout-themes/oblogout.conf.arcolinuxnew ../archiso/airootfs/etc/oblogout.conf
 echo "oblogout.conf copied"
 
-echo "getting slim.conf from archmerge-slimlock-themes"
-cp -r ../../Archmerge-Slimlock-Themes/slim.conf.archmergenew ../archiso/airootfs/etc/slim.conf
+echo "getting slim.conf from arcolinux-slimlock-themes"
+cp -r ../../arcolinux-slimlock-themes/slim.conf.arcolinuxnew ../archiso/airootfs/etc/slim.conf
 echo "slim.conf copied"
 
 
 # echo "Updating the .config folder"
 # mkdir ../archiso/airootfs/etc/skel/.config
 # echo "Copying files/folders to folder from github"
-# cp -r ../../Archmerge-Iso-Skel/config/* ../archiso/airootfs/etc/skel/.config/
+# cp -r ../../arcolinux-Iso-Skel/config/* ../archiso/airootfs/etc/skel/.config/
 # echo "Everything copied in ~/.config"
 
 
 # echo "Updating the .local folder"
 # mkdir ../archiso/airootfs/etc/skel/.local
 # echo "Copying files/folders to folder from github"
-# cp -r ../../Archmerge-Iso-Skel/local/* ../archiso/airootfs/etc/skel/.local/
+# cp -r ../../arcolinux-Iso-Skel/local/* ../archiso/airootfs/etc/skel/.local/
 # echo "Everything copied in ~/.local"
 
 
 
 # mkdir ../archiso/airootfs/etc/skel/.config/openbox
 # echo "Copying new files/folders to folders from github"
-# cp -r ../../Archmerge-Openbox-Configs/* ../archiso/airootfs/etc/skel/.config/openbox/
+# cp -r ../../arcolinux-Openbox-Configs/* ../archiso/airootfs/etc/skel/.config/openbox/
 # echo "delete junk files"
 # rm -rf ../archiso/airootfs/etc/skel/.config/openbox/git-v*.sh
 # rm -rf ../archiso/airootfs/etc/skel/.config/openbox/README.md
@@ -55,7 +55,7 @@ echo "slim.conf copied"
 # echo "Updating i3 config files"
 # mkdir ../archiso/airootfs/etc/skel/.config/i3
 # echo "Copying new files/folders to folders from github"
-# cp -r ../../Archmerge-i3wm/* ../archiso/airootfs/etc/skel/.config/i3/
+# cp -r ../../arcolinux-i3wm/* ../archiso/airootfs/etc/skel/.config/i3/
 # echo "delete junk files"
 # rm -rf ../archiso/airootfs/etc/skel/.config/i3/git-v*.sh
 # rm -rf ../archiso/airootfs/etc/skel/.config/i3/README.md
@@ -64,7 +64,7 @@ echo "slim.conf copied"
 
 # echo "Updating the conky files"
 # mkdir ../archiso/airootfs/etc/skel/.config/conky
-# cp -r ../../Archmerge-Conky-Collection/* ../archiso/airootfs/etc/skel/.config/conky/
+# cp -r ../../arcolinux-Conky-Collection/* ../archiso/airootfs/etc/skel/.config/conky/
 # echo "delete junk files"
 # rm -rf ../archiso/airootfs/etc/skel/.config/conky/git-v*.sh
 # rm -rf ../archiso/airootfs/etc/skel/.config/conky/README.md
@@ -72,5 +72,5 @@ echo "slim.conf copied"
 
 # echo "Updating the termite themes files"
 # mkdir ../archiso/airootfs/etc/skel/.config/termite/themes
-# cp -r ../../Archmerge-Termite-Themes/themes/* ../archiso/airootfs/etc/skel/.config/termite/themes
+# cp -r ../../arcolinux-Termite-Themes/themes/* ../archiso/airootfs/etc/skel/.config/termite/themes
 # echo "Everything copied in ~/.config/termite/themes"
