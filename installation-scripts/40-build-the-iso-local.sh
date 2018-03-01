06755d6adb6241a1bd64ace701b2e79d2158f2d7
Erik Dubois <erik.dubois@gmail.com>
2018-03-01 16:21:17 +0100

    script 40 - check for archiso

--- installation-scripts/40-build-the-iso-local.sh
+++ installation-scripts/40-build-the-iso-local.sh
@@ -12,6 +12,58 @@ set -e
 #
 ##################################################################################################################
 
+echo "Checking if archiso is installed"
+
+package="archiso"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+
+	elif pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	fi
+
+	# Just checking if installation was successful
+	if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "#########  "$package" has been installed"
+	echo "################################################################"
+
+	else
+
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+	fi
+
+fi
+
+
+
 echo "building local"
 
 
