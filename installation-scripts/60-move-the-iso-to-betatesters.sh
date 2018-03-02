e29ac167670f278e557e6d7c22737694e28224a7
Erik Dubois <erik.dubois@gmail.com>
2018-03-02 23:29:32 +0100

    moving scripts update

---
+++ installation-scripts/60-move-the-iso-to-betatesters.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# ArcoLinux	: 	https://arcolinux.com/
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#echo "moving iso to documents"
+#cp ~/arcolinux-build/archiso/out/arcolinux* ~/Documents/
+
+#echo "moving iso to insync just for Teammembers only"
+#cp ~/arcolinux-build/archiso/out/arcolinux* ~/Insync/SHARED/TeamMembers-Only/
+
+echo "moving iso to insync ArcoLinux-A-Team"
+cp ~/arcolinux-build/archiso/out/arcolinux* ~/Insync/SHARED/ArcoLinux-A-Team/
