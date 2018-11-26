6868035e5ac3af79f5714196f34542ed8b98d849
Erik Dubois <erik.dubois@gmail.com>
2018-11-26 10:02:27 +0100

    new version 18.12.2

--- installation-scripts/10-change-versions.sh
+++ installation-scripts/10-change-versions.sh
@@ -14,10 +14,10 @@ set -e
 
 echo "Let us change all instances of version number everywhere."
 
-echo "What is the old version? e.g.   v18.11.1    (including v and dots)"
+echo "What is the old version? e.g.   v18.12.1    (including v and dots)"
 read oldversion
 
-echo "To what version do you want upgrade e.g.    v18.11.2   (including v and dots)"
+echo "To what version do you want upgrade e.g.    v18.12.2   (including v and dots)"
 
 read newversion
 
