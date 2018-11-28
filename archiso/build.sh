6d2bb16f963c8de89ea1001e4def5458270e7ea1
Erik Dubois <erik.dubois@gmail.com>
2018-11-28 17:16:15 +0100

    version update

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux
-iso_label="arcolinux-v18.12.3-x86_64"
+iso_label="arcolinux-v18.12.4-x86_64"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v18.12.3"
+iso_version="v18.12.4"
 install_dir=arch
 work_dir=work
 out_dir=out
