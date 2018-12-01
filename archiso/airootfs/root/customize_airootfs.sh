b0bacdca8e12c1ea1607e671242ef67d79228d89
Erik Dubois <erik.dubois@gmail.com>
2018-12-01 10:50:24 +0100

    compare with arcolinuxd

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -96,7 +96,7 @@ function initkeysFunc() {
     #pacman-key --keyserver hkps://hkps.pool.sks-keyservers.net:443 -r 74F5DE85A506BF64
     #pacman-key --keyserver hkp://pool.sks-keyservers.net:80 -r 74F5DE85A506BF64
     pacman-key --lsign-key 74F5DE85A506BF64
-	  #sudo pacman-key --refresh-keys
+    #sudo pacman-key --refresh-keys
 }
 
 function getNewMirrorCleanAndUpgrade() {
