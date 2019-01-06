fc1ceadb718a7ca13ed0a76e32b73068517a5ce9
Erik Dubois <erik.dubois@gmail.com>
2019-01-06 21:51:01 +0100

    version 4

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux
-iso_label="arcolinux-v19.01.3-x86_64"
+iso_label="arcolinux-v19.01.4-x86_64"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v19.01.3"
+iso_version="v19.01.4"
 install_dir=arch
 work_dir=work
 out_dir=out
