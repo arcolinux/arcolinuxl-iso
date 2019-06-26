301f5f62d4d07a1b5d0f0a8b48106073970b0bf5
Erik Dubois <erik.dubois@gmail.com>
2019-06-26 06:42:30 +0200

    cleanup with extra cores

--- archiso/airootfs/usr/bin/arcolinux-cleanup.sh
+++ archiso/airootfs/usr/bin/arcolinux-cleanup.sh
@@ -6,4 +6,5 @@ rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
 rm /root/{.automated_script.sh,.zlogin}
 rm /etc/mkinitcpio-archiso.conf
 rm -r /etc/initcpio
+rm /usr/bin/arcolinux-all-cores.sh
 rm /usr/bin/arcolinux-cleanup.sh
