a0e58a2dd6cf97a1834f4868cc5d46b5e4b11e2f
Erik Dubois <erik.dubois@gmail.com>
2019-11-16 08:21:40 +0100

    bump version switch

--- installation-scripts/30-build-the-iso-first-time.sh
+++ installation-scripts/30-build-the-iso-first-time.sh
@@ -14,13 +14,9 @@
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+# set our parameters
 buildFolder="$HOME/arcolinux-build"
 outFolder="$HOME/ArcoLinux-Out"
-#changing to LTS
-isoNameOld="iso_name=arcolinux"
-isoNameNew="iso_name=arcolinux-lts"
-calConfOld="arcolinux-calamares-git"
-calConfNew="arcolinux-calamares-lts-git"
 
 echo
 echo "################################################################## "
@@ -32,7 +28,6 @@ rm -rf ../archiso/airootfs/etc/skel/.* 2> /dev/null
 echo "getting .bashrc from arcolinux-root"
 wget https://raw.githubusercontent.com/arcolinux/arcolinux-root/master/etc/skel/.bashrc-latest -O ../archiso/airootfs/etc/skel/.bashrc
 echo ".bashrc copied to /etc/skel"
-
 
 echo
 echo "################################################################## "
@@ -118,59 +113,10 @@ yes | sudo pacman -Scc
 
 echo
 echo "################################################################## "
-tput setaf 2;echo "Phase 5 bis : Choose whether you want linux kernel or linux-lts kernel"
-echo "If you do not know what this is about, choose linux kernel"
+tput setaf 2;echo "Phase 5 bis : Linux kernel or linux-lts kernel is chosen on the iso since 11/2019"
 echo "https://wiki.archlinux.org/index.php/Kernel";tput sgr0
 echo "################################################################## "
 echo
-echo "Choose linux kernel or linux-lts kernel"
-echo
-echo "1.  linux kernel"
-echo "2.  linux-lts kernel"
-echo "Type the number..."
-
-read CHOICE
-
-WDP=$buildFolder"/archiso"
-
-case $CHOICE in
-
-    1 )
-			echo
-			echo "################################################################## "
-			echo "You have chosen for the linux kernel"
-			echo "################################################################## "
-			echo
-      ;;
-    2 )
-			echo
-			echo "################################################################## "
-			echo "You have chosen for the linux-lts kernel"
-			echo "################################################################## "
-			echo
-			FIND="calamares"
-			REPLACE="calamares-lts"
-			sudo sed -i "s/$FIND/$REPLACE/g" $WDP/packages.x86_64
-
-			FIND=$calConfOld
-			REPLACE=$calConfNew
-			sudo sed -i "s/$FIND/$REPLACE/g" $WDP/packages.x86_64
-
-			FIND="#arcolinux-local-repo-git"
-			REPLACE="arcolinux-local-repo-git"
-			sudo sed -i "s/$FIND/$REPLACE/g" $WDP/packages.x86_64
-
-			FIND=$isoNameOld
-			REPLACE=$isoNameNew
-			sudo sed -i "s/$FIND/$REPLACE/g" $WDP/build.sh
-      ;;
-    * )
-      echo "############################################"
-      echo "Wrong choice - try again - keyboard issue?"
-      echo "############################################"
-			exit 1;
-      ;;
-esac
 
 echo
 echo "################################################################## "
