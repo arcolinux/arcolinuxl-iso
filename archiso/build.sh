9db02d8eaf5a3042afd6444fb2248aa376ede8f2
Erik Dubois <erik.dubois@gmail.com>
2019-12-10 20:15:34 +0100

    bump nr

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux
-iso_label="arcolinux-v19.12.15"
+iso_label="arcolinux-v20.1.1"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v19.12.15"
+iso_version="v20.1.1"
 install_dir=arch
 work_dir=work
 out_dir=out
