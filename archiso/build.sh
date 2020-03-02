c7a03c3ad249a0890def64bacdcf724d9e924ae9
Erik Dubois <erik.dubois@gmail.com>
2020-03-02 14:22:09 +0100

    moving from dev to stable

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux
-iso_label="arcolinux-v20.2.12"
+iso_label="arcolinux-v20.3.3"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.2.12"
+iso_version="v20.3.3"
 install_dir=arch
 work_dir=work
 out_dir=out
