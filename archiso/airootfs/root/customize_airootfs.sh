4a6b3d053c2b4ce28aff887c255f0342595e27ac
Erik Dubois <erik.dubois@gmail.com>
2020-04-15 12:02:49 +0200

    enable update mirrors service

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -87,19 +87,18 @@ function enableServicesFunc() {
 	systemctl set-default graphical.target
 	systemctl enable NetworkManager.service
 	systemctl enable virtual-machine-check.service
-	#systemctl enable reflector.service
-	#systemctl enable reflector.timer
-  	systemctl enable org.cups.cupsd.service
-  	systemctl enable bluetooth.service
-  	systemctl enable ntpd.service
-  	#systemctl enable smb.service
-  	#systemctl enable nmb.service
-  	#systemctl enable winbind.service
-  	systemctl enable avahi-daemon.service
-  	systemctl enable avahi-daemon.socket
-  	#systemctl enable tlp.service
-  	#systemctl enable tlp-sleep.service
-  	#systemctl enable vnstat.service
+	systemctl enable update-mirrors.service
+  systemctl enable org.cups.cupsd.service
+  systemctl enable bluetooth.service
+  systemctl enable ntpd.service
+  #systemctl enable smb.service
+  #systemctl enable nmb.service
+  #systemctl enable winbind.service
+  systemctl enable avahi-daemon.service
+  systemctl enable avahi-daemon.socket
+  #systemctl enable tlp.service
+  #systemctl enable tlp-sleep.service
+  #systemctl enable vnstat.service
 }
 
 function fixWifiFunc() {
