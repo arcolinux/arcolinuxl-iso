11d1a55dff62214b6745b46de7ad8a2834911b0d
Erik Dubois <erik.dubois@gmail.com>
2020-04-15 19:20:50 +0200

    version 20.5.2 for new package arcolinux-logout-git

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux
-iso_label="arcolinux-v20.5.1"
+iso_label="arcolinux-v20.5.2"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.5.1"
+iso_version="v20.5.2"
 install_dir=arch
 work_dir=work
 out_dir=out
