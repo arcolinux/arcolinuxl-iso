9c30f1c629b8accfc89900685fbe41b5390d438c
Erik Dubois <erik.dubois@gmail.com>
2020-04-15 12:34:19 +0200

    lclc_all error out

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -26,7 +26,7 @@ function localeGenFunc() {
     sed -i 's/^#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
     export LANGUAGE=en_US.UTF-8
     export LANG=en_US.UTF-8
-    export LC_ALL=en_US.UTF-8
+    #export LC_ALL=en_US.UTF-8
     locale-gen
 }
 
