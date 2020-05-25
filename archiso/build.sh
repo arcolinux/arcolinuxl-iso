23df6d88f6e0a74b997d44fa6514016f5082b255
Erik Dubois <erik.dubois@gmail.com>
2020-05-25 21:24:38 +0200

    20.6.4 - all cores

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux
-iso_label="arcolinux-v20.5.7"
+iso_label="arcolinux-v20.6.4"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.5.7"
+iso_version="v20.6.4"
 install_dir=arch
 work_dir=work
 out_dir=out
