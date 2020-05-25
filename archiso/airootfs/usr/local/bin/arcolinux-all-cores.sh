5970360790bd50695bb78e8cc0bad09c31ccdb17
Erik Dubois <erik.dubois@gmail.com>
2020-05-25 21:12:25 +0200

    zst extension to build

--- archiso/airootfs/usr/local/bin/arcolinux-all-cores.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-all-cores.sh
@@ -23,6 +23,7 @@ then
         echo "Changing the compression settings for "$numberofcores" cores."
         sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z - --threads=0)/g' /etc/makepkg.conf
         sudo sed -i 's/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q - --threads=0)/g' /etc/makepkg.conf
+        sudo sed -i "s/PKGEXT='.pkg.tar.xz'/PKGEXT='.pkg.tar.zst'/g" /etc/makepkg.conf
 else
         echo "No change."
 fi
