8ba3f777d3ca9b16e195ca650d325c006fee4595
Marco Obaid <marco.obaid@gmail.com>
2020-08-31 06:50:35 -0500

    Update dev-rel to v20.9.2

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux
-iso_label="arcolinux-v20.9.1"
+iso_label="arcolinux-v20.9.2"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.9.1"
+iso_version="v20.9.2"
 install_dir=arch
 work_dir=work
 out_dir=out
