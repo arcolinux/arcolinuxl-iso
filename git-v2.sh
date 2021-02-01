5b5db46a82db1172f975171ed7a7c5262b8cda61
Erik Dubois <erik.dubois@gmail.com>
2021-02-01 12:11:27 +0100

    new archiso version

--- git-v2.sh
+++ git-v2.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website 	: 	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# change a commit comment
+# git commit --amend -m "more info"
+# git push --force origin
 
 echo "Deleting the work folder if one exists"
 [ -d work ] && rm -rf work
