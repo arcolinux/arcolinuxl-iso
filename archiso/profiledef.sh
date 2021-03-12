014f077485e61266003c668bd224642788dcc2e9
Erik Dubois <erik.dubois@gmail.com>
2021-03-12 21:44:12 +0100

    new version for new welcome app

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name=arcolinux
-iso_label="arcolinux-v21.03.9"
+iso_label="arcolinux-v21.03.10"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v21.03.9"
+iso_version="v21.03.10"
 install_dir=arch
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
