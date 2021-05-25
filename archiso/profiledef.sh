d589e3f5c8e6bd718d3de61aa0b584e304299e55
Erik Dubois <erik.dubois@gmail.com>
2021-05-25 16:31:30 +0200

    users and arcolinux-skel and hardcode-fixer

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -24,7 +24,6 @@ file_permissions=(
   ["/etc/sudoers.d"]="0:0:750"
   ["/usr/local/bin/arcolinux-all-cores"]="0:0:755"
   ["/usr/local/bin/arcolinux-final"]="0:0:755"
-  ["/usr/local/bin/arcolinux-skel"]="0:0:755"
   ["/usr/local/bin/arcolinux-remove-nvidia"]="0:0:755"
   ["/usr/local/bin/arcolinux-displaymanager-check"]="0:0:755"
   ["/usr/local/bin/arcolinux-before"]="0:0:755"
