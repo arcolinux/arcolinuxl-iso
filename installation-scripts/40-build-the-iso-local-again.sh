d97f70b962b7d3e1d42d9fe0ebe3c1fb6ac08c2f
Erik Dubois <erik.dubois@gmail.com>
2021-06-01 12:22:41 +0200

    changes in pacman.conf for packagekit

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -34,7 +34,7 @@ echo
 	isoLabel='arcolinuxl-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 55-1"
+	archisoRequiredVersion="archiso 54-1"
 	buildFolder=$HOME"/arcolinux-build"
 	outFolder=$HOME"/ArcoLinux-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
@@ -63,7 +63,7 @@ echo
 	echo "or update your system"
 	echo "###################################################################################################"
 	tput sgr0
-	exit 1
+	#exit 1
 	fi
 
 echo
