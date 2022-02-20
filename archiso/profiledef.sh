87dc9cf7e7640087c10c0f817672dae94de9b4b3
Erik Dubois <erik.dubois@gmail.com>
2022-02-20 10:11:18 +0100

    bumpnr

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="arcolinuxl"
-iso_label="arcolinuxl-v22.03.05"
+iso_label="arcolinuxl-v22.03.06"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v22.03.05"
+iso_version="v22.03.06"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="xfce"
 	dmDesktop="xfce"
 
-	arcolinuxVersion='v22.03.05'
+	arcolinuxVersion='v22.03.06'
 
 	isoLabel='arcolinuxl-'$arcolinuxVersion'-x86_64.iso'
 
