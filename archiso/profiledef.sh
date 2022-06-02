244f332cc26bc3dcbfe22befd6c918bb7fab07ce
arcolinuxz <arcolinuxinfo@gmail.com>
2022-06-02 07:28:46 +0200

    update

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -8,14 +8,16 @@ iso_application="ArcoLinux Live/Rescue CD"
 iso_version="v22.07.02"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+	   'uefi-ia32.grub.esp' 'uefi-x64.systemd-boot.esp'
+	   'uefi-ia32.grub.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/gshadow"]="0:0:600"
-  ["/etc/shadow"]="0:0:600"
+  ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/etc/polkit-1/rules.d"]="0:0:750"
