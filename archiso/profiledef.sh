005a7c7119e8ec112d8b0b9bfa789f89cd93ab0a
arcolinuxz <arcolinuxinfo@gmail.com>
2022-06-26 08:21:28 +0200

    new calamares css

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="arcolinuxl"
-iso_label="arcolinuxl-v22.07.03"
+iso_label="arcolinuxl-v22.09.01"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v22.07.03"
+iso_version="v22.09.01"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
